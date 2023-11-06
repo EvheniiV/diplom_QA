30ad43fdaa57abb7ebba6efa9ef69d5c34530481
EvheniiV <jenyokgm@gmail.com>
2023-11-06 23:32:08 +0300

    begin test writing

--- src/test/java/ru/netology/test/TestByDebit.java
+++ src/test/java/ru/netology/test/TestByDebit.java
@@ -1,4 +1,38 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.pages.MainPage;
+import ru.netology.pages.PaymentFormPageDebit;
+import ru.netology.data.SqlHelper;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestByDebit {
+
+    private MainPage mainPage;
+    private PaymentFormPageDebit paymentFormPageDebit;
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        mainPage = open("http://localhost:8080/", MainPage.class);
+    }
+
+    @AfterEach
+    public void cleanDB() {
+        SqlHelper.clearTables();
+    }
 }
